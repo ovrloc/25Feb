a58a6f1f1497e9db170a66ee2690ace1d28f5331
BILLIONAIRE <lokenome@gmail.com>
2019-03-13 19:32:29 +0200

    first

---
+++ app/src/main/java/com/billionaire/chatapp/Fragments/APIService.java
@@ -0,0 +1,21 @@
+package com.billionaire.chatapp.Fragments;
+
+import com.billionaire.chatapp.Notifications.MyResponse;
+import com.billionaire.chatapp.Notifications.Sender;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @Headers(
+            {
+                    "Content-Type:application/json",
+                    "Authorization:key=ADD HERE YOUR KEY FROM FIREBASE PROJECT"
+            }
+    )
+
+    @POST("fcm/send")
+    Call<MyResponse> sendNotification(@Body Sender body);
+}
